4150f3f8f793f7e9ab400082d16427eebec56d63
Manohar Prajapat <43112633+manohar7470@users.noreply.github.com>
2023-08-10 00:15:13 +0530

    Add files via upload

---
+++ CourseController.java
@@ -0,0 +1,22 @@
+package com.code.springboot.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+	@RequestMapping("/courses")
+	public List<Course> retreiveAllCourses(){
+		return Arrays.asList(
+				new Course(1,"Learn SpringBoot", "in21minutes"),
+				new Course(2,"Learn AWS Genesyss", "in21minutes"),
+				new Course(3,"Learn JNDI", "in21minutes")
+				//new Course(3,"Learn Microservices", "in21minutes")
+				
+				);
+	}
+
+}
---
+++ LearnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.code.springboot.learnspringboot;
+//http://localhost:9090/courses
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootApplication.class, args);
+	}
+
+}
